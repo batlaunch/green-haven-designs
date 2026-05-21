008f13c1d2aeebaa54a70121d8d2a8fa3f1b76df
timmyc181 <timothy.j.cleveland1@gmail.com>
2026-05-20 23:30:53 -0400

    Remove server.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,44 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  tanstackStart: {
-    spa: { enabled: true },
-    prerender: { enabled: true, crawlLinks: true },
-  },
+export default defineConfig(({ mode }) => {
+  const loadedEnv = loadEnv(mode, process.cwd(), "VITE_");
+  const envDefine = Object.fromEntries(
+    Object.entries(loadedEnv).map(([key, value]) => [
+      `import.meta.env.${key}`,
+      JSON.stringify(value),
+    ]),
+  );
+
+  return {
+    define: envDefine,
+    resolve: {
+      alias: {
+        "@": `${process.cwd()}/src`,
+      },
+      dedupe: [
+        "react",
+        "react-dom",
+        "react/jsx-runtime",
+        "react/jsx-dev-runtime",
+        "@tanstack/react-query",
+        "@tanstack/query-core",
+      ],
+    },
+    server: {
+      host: "::",
+      port: 8080,
+    },
+    plugins: [
+      tanstackRouter({
+        target: "react",
+      }),
+      tailwindcss(),
+      tsconfigPaths({ projects: ["./tsconfig.json"] }),
+      react(),
+    ],
+  };
 });
